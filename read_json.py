89d740f5cd7b35f8bf1d6710362d9a7612052b91
isakheen <104990201+isakheen@users.noreply.github.com>
2023-12-03 16:28:27 +0100

    Update read_json.py
    Added update_inventory

--- read_json.py
+++ read_json.py
@@ -1,24 +1,53 @@
 import json
 
+
+def update_inventory(color, quantity, operation):
+    file = open("inventory.json", "r")
+    data = json.load(file)
+    file.close()
+    old_string = str(data[order])
+    old_quantity = int(old_string[-3])
+    if operation == "add": #if inleverans
+        new_quantity = str(old_quantity + quantity)
+    if operation == "sub": #if packning
+        new_quantity = str(old_quantity - quantity)
+
+    if int(new_quantity) > 9:
+        print("Inventory full")
+
+    if int(new_quantity) < 0:
+        print("Invalid quantity")
+    new_string = {"Name": "" + color + "", "Quantity": "" + new_quantity + ""}
+    data[order] = new_string
+    file = open("inventory.json", "w+")
+    file.write(json.dumps(data))
+    file.close()
+
+
 # Kan läsa både inventory.json och orders.json
 with open("orders.json", "r") as file:
     file_content = json.load(file)
     for order in range(len(file_content)):
         order_string = str(file_content[order])
         if "Green" in order_string:
-            color = "green"
-            color_quantity = order_string[-3:-2]
+            color = "Green Cube"
+            color_quantity = int(order_string[-3])
+            update_inventory(color, color_quantity, "sub")
 
         if "Blue" in order_string:
-            color = "blue"
-            color_quantity = order_string[-3:-2]
+            color = "Blue Cube"
+            color_quantity = int(order_string[-3])
+            update_inventory(color, color_quantity, "add")
 
         if "Yellow" in order_string:
-            color = "yellow"
-            color_quantity = order_string[-3:-2]
+            color = "Yellow Cube"
+            color_quantity = int(order_string[-3])
+            update_inventory(color, color_quantity, "add")
 
         if "Red" in order_string:
-            color = "red"
-            color_quantity = order_string[-3:-2]
+            color = "Red Cube"
+            color_quantity = int(order_string[-3])
+            update_inventory(color, color_quantity, "add")
 
-        print([color, color_quantity])
+        #print([color, color_quantity])
+
